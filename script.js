38b93ab3b5d12571bc02e23fc79bf5f4391a42dc
LenaBruch <48550967+LenaBruch@users.noreply.github.com>
2019-03-28 21:04:22 +0100

    Week3 upload

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log('app is alive')
+
+function switchChannel (SevenContinents) {
+Console('Tuning into channel' + SevenContinents);
+$('#channelName').innerHTML='Yummy';
+$('#location').innerHTML='upgrading.never.helps';
+$('currentChatFavorite').attr(src="http://ip.lfe.mw.tum.de/sections/star-o.png", alt="not starred");
+toggleButtons();}
+
+switchChannel.apply(onclick)
+
+function toggleButtons () {
+    $('.selected').toggle();
+    
+}
+
+onclick('#emojis-button').toggle('#emojis')
